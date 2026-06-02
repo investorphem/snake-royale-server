5c9a31b2698df1e6b7879b30cf8778312818a91e
Oluwafemi <dracuul54@gmail.com>
2026-06-02 03:23:55 +0100

    fix: swap celo testnet rpc to ankr

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const io = new Server(server, {
 
 // --- WEB3 SETTLEMENT SETUP ---
 // Connect to Celo Alfajores Testnet
-const provider = new ethers.JsonRpcProvider("https://alfajores-forno.celo-testnet.org");
+const provider = new ethers.JsonRpcProvider("https://rpc.ankr.com/celo_testnet");
 const wallet = new ethers.Wallet(process.env.ADMIN_PRIVATE_KEY, provider);
 
 const CONTRACT_ADDRESS = "YOUR_WAGER_CONTRACT_ADDRESS"; // Paste your contract address here
